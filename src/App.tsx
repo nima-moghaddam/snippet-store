90a55fecf1d0f8a5e4d6735e2439352dbf5d143b
nima <84495449+nima-moghaddam@users.noreply.github.com>
2024-07-25 10:09:39 +0330

    add syntax highlighter package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,59 @@
+import SyntaxHighlighterWrapper from "./components/syntax-highlighter"
 
 function App() {
-  return <>app</>
+  const codeString = `
+  const useMutate = ({
+    method,
+    url,
+    successCallback,
+    errorCallback,
+    header = {},
+    customBaseUrl,
+    preventDefaultMessage = false
+  }: MutateQuery) => {
+    const toast = useToast()
+  
+    return useMutation({
+      mutationFn: async ({ id, query, requestUrl }: QueryParam) => {
+        const requestOptions = {
+          method,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: \`Bearer \${swid}\`,
+            ...header
+          },
+          body: JSON.stringify(query)
+        }
+  
+        const urlPath = requestUrl || \`\${url}\${id ? \`/\${id}\` : ""}\`
+  
+        try {
+          const response = await fetch(\`\${customBaseUrl ?? BaseURL}/\${urlPath}\`, requestOptions)
+  
+          if (!response.ok) {
+            throw new Error("Server has encountered a problem")
+          }
+  
+          const parsedResponse = await response.json()
+          const data = parsedResponse.data
+  
+          return { ...data, statusMessage: parsedResponse.message }
+        } catch (error: any) {
+          throw new Error(error.message)
+        }
+      },
+      onSuccess: (data) => {
+        successCallback?.(data)
+        !preventDefaultMessage && toast("Success", data.statusMessage)
+      },
+      onError: (error: any) => {
+        errorCallback?.(error.message)
+        !preventDefaultMessage && toast("Error", error.message)
+      }
+    })
+  }
+  `
+  return <SyntaxHighlighterWrapper>{codeString}</SyntaxHighlighterWrapper>
 }
 
 export default App
