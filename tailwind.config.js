6c41a760d47bc15074332e10a95248e8a5846600
nima <84495449+nima-moghaddam@users.noreply.github.com>
2024-08-17 13:18:23 +0330

    update tailwind theme for new ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         dark: "#0e2432",
         dark_lighter: "#373c42",
-        blue: "#339ae0",
         ivory: "#fefefe",
         pink: "#ff4181",
         gray: "#dfd1c5",
-        orange: "#f9cd61"
+        orange: "#f9cd61",
+        //-------
+        primary: "#f8f9fb",
+        secondary: "#c80fa0",
+        "font-dark": "#3b4560",
+        "font-normal": "#65758c",
+        "font-light": "#a7a7ab",
+        "font-ultra-light": "#ced2d4",
+        green: "#acdc3c",
+        red: "#b81f36",
+        blue: "#1f9cfe",
       },
       boxShadow: {
-        '3xl': '0 4px 8px 0 rgba(0, 0, 0, 1), 0 6px 20px 0 rgba(0, 0, 0, 0.19);',
-      }
+        "3xl":
+          "0 4px 8px 0 rgba(0, 0, 0, 1), 0 6px 20px 0 rgba(0, 0, 0, 0.19);",
+        card: "0 20px 27px 0 rgba(0, 0, 0, .05)",
+        icon: "0 .3125rem .625rem 0 rgba(0,0,0,.12)",
+      },
+      backgroundImage: {
+        "primary-gradient": "linear-gradient(310deg, #7928ca, #ff0080)",
+      },
     },
     screens: {
       xs: "360px",
@@ -27,10 +39,8 @@ export default {
       lg: "1100px",
       xl: "1224px",
       "2xl": "1200px",
-      "3xl": "1300px"
-    }
+      "3xl": "1300px",
+    },
   },
-  plugins: []
-}
-
-
+  plugins: [],
+};
