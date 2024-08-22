9592a02d842a003e403e41e3edcb4c61619f1341
nima <84495449+nima-moghaddam@users.noreply.github.com>
2024-08-22 11:51:22 +0330

    snippet detail ui update

--- src/modules/snippet-detail/index.tsx
+++ src/modules/snippet-detail/index.tsx
@@ -11,6 +11,7 @@ import { IoCameraOutline } from "react-icons/io5";
 import { RouteEnum } from "../../types/RouteModels";
 import useAnimation from "../../utils/useAnimation";
 import { ImLink } from "react-icons/im";
+import Card from "../../components/card/Card";
 
 const SnippetDetailPage = () => {
   const { pathname } = useLocation();
@@ -38,72 +39,78 @@ const SnippetDetailPage = () => {
   };
 
   return (
-    <section className={`${animateClass}`}>
-      <div className="mb-1 border-b border-slate-300 pb-2">
-        <h1 className="text-[1.5rem] font-semibold md:text-[2rem]">
-          {snippet?.title}
-        </h1>
-      </div>
-
-      <div className="mb-5 flex flex-col items-start justify-between sm:flex-row sm:items-center">
-        <div className="flex flex-wrap">
-          {snippet?.tags?.map((tag) => (
-            <div
-              className="me-1 cursor-pointer text-blue hover:text-pink"
-              onClick={() => handleTagFilter(tag)}
-            >
-              #{tag}
-            </div>
-          ))}
+    <>
+      <Card classes={`${animateClass} mb-4`}>
+        <div className="mb-1 border-b border-gray-lighter pb-2">
+          <h1 className="text-lg font-bold text-gray-dark md:text-xl">
+            {snippet?.title}
+          </h1>
         </div>
-        <div className="flex items-center">
-          {snippet?.link && (
-            <button
-              onClick={() => window.open(snippet?.link)}
-              className="flex cursor-pointer items-center justify-center px-2 py-1 hover:text-pink"
-            >
-              <ImLink className="me-2 h-4 w-4" />
-              <span>Link</span>
-            </button>
-          )}
-          <button
-            onClick={() => window.open(`preview/${snippet?.title}`)}
-            className="flex cursor-pointer items-center justify-center px-2 py-1 hover:text-pink"
-          >
-            <IoCameraOutline className="me-2 h-5 w-5" />
-            <span>Photo</span>
-          </button>
-          <button
-            onClick={handleCodeCopy}
-            className="flex cursor-pointer items-center justify-center px-2 py-1 hover:text-pink"
-          >
-            <MdContentCopy className="me-2 h-4 w-4" />
-            <span>Copy</span>
-          </button>
-        </div>
-      </div>
 
-      {snippet && (
-        <div className="mb-10 flex w-full flex-col">
-          <div className="overflow-hidden rounded-md">
-            <SyntaxHighlighterWrapper>{snippet.code}</SyntaxHighlighterWrapper>
+        <div className="mb-5 flex flex-col items-start justify-between sm:flex-row sm:items-center">
+          <div className="flex flex-wrap">
+            {snippet?.tags?.map((tag) => (
+              <div
+                className="me-1 cursor-pointer text-blue hover:text-secondary"
+                onClick={() => handleTagFilter(tag)}
+              >
+                #{tag}
+              </div>
+            ))}
           </div>
-          {snippet?.type && (
-            <div className="mt-5 overflow-hidden rounded-md">
+          <div className="flex items-center">
+            {snippet?.link && (
+              <button
+                onClick={() => window.open(snippet?.link)}
+                className="flex cursor-pointer items-center justify-center px-2 py-1 text-gray-dark hover:text-secondary"
+              >
+                <ImLink className="me-2 h-4 w-4" />
+                <span>Link</span>
+              </button>
+            )}
+            <button
+              onClick={() => window.open(`preview/${snippet?.title}`)}
+              className="flex cursor-pointer items-center justify-center px-2 py-1 text-gray-dark hover:text-secondary"
+            >
+              <IoCameraOutline className="me-2 h-5 w-5" />
+              <span>Photo</span>
+            </button>
+            <button
+              onClick={handleCodeCopy}
+              className="flex cursor-pointer items-center justify-center px-2 py-1 text-gray-dark hover:text-secondary"
+            >
+              <MdContentCopy className="me-2 h-4 w-4" />
+              <span>Copy</span>
+            </button>
+          </div>
+        </div>
+
+        {snippet && (
+          <div className="mb-10 flex w-full flex-col">
+            <div className="overflow-hidden rounded-md">
               <SyntaxHighlighterWrapper>
-                {snippet.type}
+                {snippet.code}
               </SyntaxHighlighterWrapper>
             </div>
-          )}
-        </div>
-      )}
+            {snippet?.type && (
+              <div className="mt-5 overflow-hidden rounded-md">
+                <SyntaxHighlighterWrapper>
+                  {snippet.type}
+                </SyntaxHighlighterWrapper>
+              </div>
+            )}
+          </div>
+        )}
+      </Card>
       {snippet?.description && (
-        <>
-          <div className="mb-1 underline underline-offset-2">Description</div>
-          <p className="ps-3 gray-light">{snippet?.description}</p>
-        </>
+        <Card>
+          <div className="mb-2 text-lg font-bold text-gray-dark">
+            Description
+          </div>
+          <p className="text-base text-gray">{snippet?.description}</p>
+        </Card>
       )}
-    </section>
+    </>
   );
 };
 
